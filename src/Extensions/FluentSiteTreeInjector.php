c5c9edeb16f340a0e6fd35a3abd304e8d15a5e82
innomedia <innomedia>
2020-10-06 12:04:09 +0200

    overwritten original updateLink function

---
+++ src/Extensions/FluentSiteTreeInjector.php
@@ -0,0 +1,103 @@
+<?php
+namespace SubsiteFluentExtensions;
+
+use SilverStripe\CMS\Forms\SiteTreeURLSegmentField;
+use SilverStripe\CMS\Model\SiteTree;
+use SilverStripe\Control\Controller;
+use SilverStripe\Control\Director;
+use SilverStripe\Forms\FieldList;
+use SilverStripe\ORM\DB;
+use TractorCow\Fluent\Extension\FluentSiteTreeExtension;
+use TractorCow\Fluent\Model\Locale;
+use TractorCow\Fluent\State\FluentState;
+
+class FluentSiteTreeInjector extends FluentSiteTreeExtension
+{
+    protected function addLocalePrefixToUrlSegment(FieldList $fields)
+    {
+
+        // Ensure the field is available in the list
+        $segmentField = $fields->fieldByName('Root.Main.URLSegment');
+        if (!$segmentField || !($segmentField instanceof SiteTreeURLSegmentField)) {
+            return $this;
+        }
+        // Mock frontend and get link to parent object / page
+        $baseURL = FluentState::singleton()
+            ->withState(function (FluentState $tempState) {
+                $tempState->setIsDomainMode(true);
+                $tempState->setIsFrontend(true);
+
+                // Get relative link up until the current URL segment
+                if (SiteTree::config()->get('nested_urls') && $this->owner->ParentID) {
+                    $parentRelative = $this->owner->Parent()->RelativeLink();
+                } else {
+                    $parentRelative = '/';
+                    $action = null;
+                    $this->updateRelativeLink($parentRelative, $action);
+                }
+                if ($this->owner->SubsiteID == 0) {
+                    $domain = Locale::getCurrentLocale()->getDomain();
+                    if ($domain) {
+                        $parentBase = Controller::join_links($domain->Link(), Director::baseURL());
+                    } else {
+                        $parentBase = Director::absoluteBaseURL();
+                    }
+                } else {
+                    $Locale = Locale::getCurrentLocale()->Locale;
+                    $SubsiteID = $this->owner->SubsiteID;
+                    $parentBase = $this->getSubsiteDomainByLocale($Locale,$SubsiteID);
+                    
+                }
+                // Get absolute base path
+
+                // Join base / relative links
+                return Controller::join_links($parentBase, $parentRelative);
+            });
+
+        //TODO add Extension point here in fork then PR
+        $segmentField->setURLPrefix($baseURL);
+        return $this;
+    }
+    public function updateLink(&$link, &$action, &$relativeLink)
+    {
+
+        // Get appropriate locale for this record
+        if ($this->owner->SubsiteID == 0) {
+            parent::updateLink($link,$action,$relativeLink);
+        }
+        else
+        {
+            $Locale = Locale::getCurrentLocale()->Locale;
+            $SubsiteID = $this->owner->SubsiteID;
+            $SubsiteBaseLink = $this->getSubsiteDomainByLocale($Locale,$SubsiteID);
+            if($SubsiteBaseLink != "")
+            {
+
+            }
+            if(strpos($link,$Locale) !== false)
+            {
+                $link = str_replace($Locale."/","",$link);
+            }
+            
+            $link = Controller::join_links($SubsiteBaseLink,$link);
+        }
+    }
+    private function getSubsiteDomainByLocale($Locale,$SubsiteID)
+    {
+        $subsiteDomain = DB::query("SELECT sd.Domain From SubsiteDomain sd WHERE sd.SubsiteID = $SubsiteID AND sd.Locale = '$Locale'")->value();
+        if ($subsiteDomain != null && $subsiteDomain != "") {
+            if (!strpos($subsiteDomain, "https://") && !strpos($subsiteDomain, "http://")) {
+                $preHost = "http://";
+                if (Director::is_https()) {
+                    $preHost = "https://";
+                }
+                $subsiteDomain = $preHost . $subsiteDomain;
+            }
+
+            $parentBase .= Controller::join_links($subsiteDomain, Director::baseURL());
+        } else {
+            $subsiteDomain = Director::absoluteBaseURL();
+        }
+        return $subsiteDomain;
+    }
+}
